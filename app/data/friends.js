c15de68d46b9ac53edb12d3d48b443f1711ec933
doncasanova <ethanjuan@yahoo.com>
2018-06-03 10:02:49 -0500

    added logic to find friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,12 @@
+// import { stringify } from "querystring";
+
 // Friends list (DATA)
 // =============================================================
 var friends = [
     {
-        name:"Ahmed",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        scores:[
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
             5,
             1,
             4,
@@ -15,12 +17,30 @@ var friends = [
             5,
             4,
             1
-          ]
-      },
-      {
-        name:"Ahmed",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        scores:[
+        ],
+        compatNum: "32"
+    },
+    {
+        name: "Ahmed2",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            3,
+            4,
+            5,
+            5,
+            2,
+            2,
+            5,
+            4,
+            1
+        ],
+        compatNum: "25"
+    },
+    {
+        name: "Ahmed3",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
             5,
             1,
             4,
@@ -28,25 +48,69 @@ var friends = [
             5,
             1,
             2,
-            5,
-            4,
-            1
-          ]
-      },
-      {
-        name:"Ahmed",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        scores:[
-            5,
             1,
-            4,
-            4,
-            5,
             1,
-            2,
-            5,
-            4,
             1
-          ]
-      }
-  ];
+        ],
+        compatNum: "37"
+    }
+];
+
+
+var compatNum = 0
+function getCompatNum() {
+    var friendsGuesses = friends[2].scores;
+    for (var i = 0; i < friendsGuesses.length; i++) {
+        compatNum += friendsGuesses[i]
+    }
+    // console.log(friendsScore)
+};
+
+function compareScores() {
+    var scoreList = [0];
+    for (var i = 1; i < friends.length; i++) {
+        test = (friends[i].compatNum);
+        // console.log(test)
+        scoreList.push(test);
+
+
+    }
+    var yourNum = (friends[0].compatNum); 
+    smallestDiff = Math.abs(yourNum - scoreList[0]);
+    closest = 0; //index of the current closest number
+
+    for (i = 1; i < scoreList.length; i++) {
+        currentDiff = Math.abs(yourNum - scoreList[i]);
+        if (currentDiff < smallestDiff) {
+            smallestDiff = currentDiff;
+            closest = i;
+           
+        }
+       var yourFriend = closest
+    }
+   
+   
+    console.log('This is your number  ' + yourNum)
+    console.log("This is your match  " + scoreList[closest]);
+    console.log("This is your match  " + friends[yourFriend]);
+};
+compareScores()
+// yourMatch()
+
+
+// var match = yourMatch(function() {
+//     var yourNum = (friends[0].compatNum); 
+//     smallestDiff = Math.abs(yourNum - scoreList[0]);
+//     closest = 0; //index of the current closest number
+
+//     for (i = 1; i < scoreList.length; i++) {
+//         currentDiff = Math.abs(yourNum - scoreList[i]);
+//         if (currentDiff < smallestDiff) {
+//             smallestDiff = currentDiff;
+//             closest = i;
+//         }
+//     }
+//     console.log(scoreList[closest]);
+
+// });
+
--- server.js
+++ server.js
@@ -14,55 +14,57 @@ app.use(bodyParser.json());
 
 var friends = [
   {
-      name:"Ahmed",
-      photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-      scores:[
-          5,
-          1,
-          4,
-          4,
-          5,
-          1,
-          2,
-          5,
-          4,
-          1
-        ]
-    },
-    {
-      name:"Ahmed",
-      photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-      scores:[
-          5,
-          1,
-          4,
-          4,
-          5,
-          1,
-          2,
-          5,
-          4,
-          1
-        ]
-    },
-    {
-      name:"Ahmed",
-      photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-      scores:[
-          5,
-          1,
-          4,
-          4,
-          5,
-          1,
-          2,
-          5,
-          4,
-          1
-        ]
-    }
+    name: "Ahmed1",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ],
+    compatNum: "32"
+  },
+  {
+    name: "Ahmed2",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+      5,
+      3,
+      4,
+      5,
+      5,
+      2,
+      2,
+      5,
+      4,
+      1
+    ],
+    compatNum: "36"
+  },
+  {
+    name: "Ahmed3",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      1,
+      1,
+      1
+    ],
+    compatNum: "25"
+  }
 ];
-
 
 
 
@@ -71,19 +73,82 @@ app.get("/", function (req, res) {
 });
 
 
-app.get("/survey", function(req, res) {
+app.get("/survey", function (req, res) {
   res.sendFile(path.join(__dirname, "app/public/survey.html"));
 });
 
 
 // Displays all friends
-app.get("/api/friends", function(req, res) {
-  return res.json(friends);
+app.get("/api/friends", function (req, res) {
+  return res.json(friends[1].scores);
+  // return (friends[1].scores);
 });
 
 
-  // Listener
+// Listener
 // ===========================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
+
+app.get("/your/friends", function (req, res) {
+  var yourNum = (friends[0].compatNum);
+  var scoreList = [0];
+  for (var i = 1; i < friends.length; i++) {
+    test = (friends[i].compatNum);
+    // console.log(test)
+    scoreList.push(test);
+
+
+  }
+
+  smallestDiff = Math.abs(yourNum - scoreList[0]);
+  closest = 0; //index of the current closest number
+
+  for (i = 1; i < scoreList.length; i++) {
+    currentDiff = Math.abs(yourNum - scoreList[i]);
+    if (currentDiff < smallestDiff) {
+      smallestDiff = currentDiff;
+      closest = i;
+
+    }
+    var yourFriend = closest
+  }
+
+
+  // console.log('This is your number  ' + yourNum)
+  // console.log("This is your match  " + scoreList[closest]);
+  // console.log("This is your match  " + friends[yourFriend]);
+
+  return res.json(friends[yourFriend].photo);
+  // return (friends[1].scores);
+});
+
+function compareScores() {
+  var scoreList = [0];
+  for (var i = 1; i < friends.length; i++) {
+    test = (friends[i].compatNum);
+    // console.log(test)
+    scoreList.push(test);
+
+
+  }
+
+  smallestDiff = Math.abs(yourNum - scoreList[0]);
+  closest = 0; //index of the current closest number
+
+  for (i = 1; i < scoreList.length; i++) {
+    currentDiff = Math.abs(yourNum - scoreList[i]);
+    if (currentDiff < smallestDiff) {
+      smallestDiff = currentDiff;
+      closest = i;
+
+    }
+    var yourFriend = closest
+  }
+
+
+  console.log('This is your number  ' + yourNum)
+  console.log("This is your match  " + scoreList[closest]);
+  console.log("This is your match  " + friends[yourFriend]);
+};
