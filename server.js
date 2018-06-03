66004f483ee5667f20d11d8989dc1fff58288955
doncasanova <ethanjuan@yahoo.com>
2018-06-02 22:00:21 -0500

    added a few names for seed

--- server.js
+++ server.js
@@ -12,13 +12,76 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+var friends = [
+  {
+      name:"Ahmed",
+      photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      scores:[
+          5,
+          1,
+          4,
+          4,
+          5,
+          1,
+          2,
+          5,
+          4,
+          1
+        ]
+    },
+    {
+      name:"Ahmed",
+      photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      scores:[
+          5,
+          1,
+          4,
+          4,
+          5,
+          1,
+          2,
+          5,
+          4,
+          1
+        ]
+    },
+    {
+      name:"Ahmed",
+      photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      scores:[
+          5,
+          1,
+          4,
+          4,
+          5,
+          1,
+          2,
+          5,
+          4,
+          1
+        ]
+    }
+];
+
+
+
+
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "app/public/home.html"));
 });
 
+
 app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname, "app/public/survey.html"));
 });
+
+
+// Displays all friends
+app.get("/api/friends", function(req, res) {
+  return res.json(friends);
+});
+
+
   // Listener
 // ===========================================================
 app.listen(PORT, function() {
